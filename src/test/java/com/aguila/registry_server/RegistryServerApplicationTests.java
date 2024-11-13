d2b40e9792d913c2d21529b8f62dcb8ec97d2917
erickaguila <erick.aguila55@gmail.com>
2024-11-12 22:38:42 -0300

    first commit

---
+++ src/test/java/com/aguila/registry_server/RegistryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aguila.registry_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
